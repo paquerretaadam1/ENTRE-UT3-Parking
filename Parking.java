e43388248dcc01edf6a66658f79ffb1de8e57146
Pedro Aquerreta <paquerretaadam1@gmail.com>
2020-10-12 16:59:18 +0200

    Definidas variables y constantes, además de el accesor y mutador del nombre del parking

--- Parking.java
+++ Parking.java
@@ -6,18 +6,51 @@
  * cerca del parking, aparcan un nº elevado de horas y se benefician de esta 
  * tarifa más económica
  * (leer enunciado)
- * 
+ * @Autor Pedro José Aquerreta Ávila
  */
 public class Parking
 {
-    
+    private final char REGULAR = 'R';
+    private final char COMERCIAL = 'C';
+
+    private final double PRECIO_BASE_REGULAR = 2;
+    private final double PRECIO_MEDIA_REGULAR_HASTA11 = 3;
+    private final double PRECIO_MEDIA_REGULAR_DESOPUES11 = 5;
+
+    private final double HORA_INICIO_ENTRADA_TEMPRANA = 6 * 60;
+    private final double HORA_FIN_ENTRADA_TEMPRANA = 8 * 60 + 30;
+    private final double HORA_INICIO_SALIDA_TEMPRANA = 15 * 60;
+    private final double HORA_FIN_SALIDA_TEMPRANA  = 18 * 60;
+    private final double PRECIO_TARIFA_PLANA_REGULAR  = 15;
+
+    private final double PRECIO_PRIMERAS3_COMERCIAL = 5;
+    private final double PRECIO_MEDIA_COMERCIAL = 3;
+
+    private String nombre;
+    private String cliente;
+    private double importeTotal;
+    private int regular;
+    private int comercial;
+    private int clientesLunes;
+    private int clientesSabado;
+    private int clientesDomingo;
+    private int clientesMaximoComercial;
+    private double importeMaximoComercial;
     /**
      * Inicializa el parking con el nombre indicada por el parámetro.
      * El resto de atributos se inicializan a 0 
      */
-    public Parking() {
-
-       
+    public Parking(String queNombre) {
+        nombre = queNombre;
+        cliente = "";
+        importeTotal = 0;
+        regular = 0;
+        comercial = 0;
+        clientesLunes = 0;
+        clientesSabado = 0;
+        clientesDomingo = 0;
+        clientesMaximoComercial = 0;
+        importeMaximoComercial = 0;
 
     }
 
@@ -25,17 +58,18 @@ public class Parking
      * accesor para el nombre del parking
      *  
      */
-    public          () {
-         
+    public String getNombre() {
+        return nombre;
     }
-    
+
     /**
      * mutador para el nombre del parking
      *  
      */
-    public  () {
-        
+    public void setNombre(String queNombre) {
+        nombre = queNombre;
     }
+
 
     /**
      *  Recibe cuatro parámetros que supondremos correctos:
@@ -75,7 +109,7 @@ public class Parking
      *  Calcula y devuelve un String que representa el nombre del día
      *  en el que más clientes han utilizado el parking - "SÁBADO"   "DOMINGO" o  "LUNES"
      */
-    public        diaMayorNumeroClientes() {
+    public void diaMayorNumeroClientes() {
 
         
 
